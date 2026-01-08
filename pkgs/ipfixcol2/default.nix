8befcef8eec5a728b9c140f5f33e944d0731cf22
Jaroslav Pesek <pesekja8@fit.cvut.cz>
2026-01-08 16:05:34 +0100

    feat(ipfixcol2): update

--- pkgs/ipfixcol2/default.nix
+++ pkgs/ipfixcol2/default.nix
@@ -11,7 +11,8 @@
   lz4,
   nemea-framework,
   git,
-  cacert
+  cacert,
+  protobuf
 }:
 
 stdenv.mkDerivation rec {
@@ -19,15 +20,15 @@ stdenv.mkDerivation rec {
   version = "2.7.1";
 
   src = fetchFromGitHub {
-    owner = "CESNET";
+    owner = "jaroslavpesek";
     repo = "ipfixcol2";
     #tag = "v${version}";
-    rev = "80c389b43e12cfaffa2ea622b829a6371b7d054c";
-    hash = "sha256-xjYuAMAME6Bhb4h3iFHf1RkTmsUcjhoJyfgorktDDc0=";
+    rev = "95e34dce62693b26fac725bd6ddf14a1f12e03ee";
+    hash = "sha256-sPOFlXqpcT8Y8JCvGJdGxmw4gDhvFH5hIZ2TPFeEF38=";
   };
 
   nativeBuildInputs = [ cmake git cacert ];
-  buildInputs = [ libfds docutils libxml2 rdkafka zlib lz4 nemea-framework ];
+  buildInputs = [ libfds docutils libxml2 rdkafka zlib lz4 nemea-framework protobuf ];
 
   postInstall = ''
     cd ../extra_plugins/output/unirec
@@ -43,6 +44,15 @@ stdenv.mkDerivation rec {
     make
     echo "Make install cliuckhouse"
     make install
+    cd ../../protobuf-kafka
+    mkdir build
+    cd build
+    echo "Building Protobuf-Kafka plugin"
+    cmake .. -DCMAKE_INSTALL_PREFIX=$out -DCMAKE_C_FLAGS="$CFLAGS -Wno-error=implicit-function-declaration" -DCMAKE_INSTALL_LIBDIR=lib
+    make
+    echo "Make install protobuf-kafka"
+    make install
+
   '';
 
   meta = {
