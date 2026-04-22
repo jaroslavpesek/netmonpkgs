c48562ea778ca67eba2fa46526b7660838210725
Jaroslav Pesek <pesekja8@fit.cvut.cz>
2026-04-22 21:27:57 +0200

    feat(ipfixcol2): update

--- pkgs/ipfixcol2/default.nix
+++ pkgs/ipfixcol2/default.nix
@@ -17,43 +17,40 @@
 
 stdenv.mkDerivation rec {
   pname = "ipfixcol2";
-  version = "2.7.1";
+  version = "2.8.0";
 
   src = fetchFromGitHub {
-    owner = "jaroslavpesek";
+    owner = "cesnet";
     repo = "ipfixcol2";
     #tag = "v${version}";
-    rev = "95e34dce62693b26fac725bd6ddf14a1f12e03ee";
-    hash = "sha256-sPOFlXqpcT8Y8JCvGJdGxmw4gDhvFH5hIZ2TPFeEF38=";
+    rev = "85009a8ac2c2a29a7eba073a644a46237ee480e5";
+    hash = "sha256-DDCGbcqA3uUgg/9CAi+SkuiPsgaKavAo+vxCW43rydo=";
   };
 
   nativeBuildInputs = [ cmake git cacert ];
   buildInputs = [ libfds docutils libxml2 rdkafka zlib lz4 nemea-framework protobuf ];
-
+  cmakeFlags = [
+    "-DCMAKE_C_FLAGS=-Wno-error=implicit-function-declaration"
+  ]; 
+ 
   postInstall = ''
-    cd ../extra_plugins/output/unirec
-    mkdir build
-    cd build
-    cmake .. -DCMAKE_INSTALL_PREFIX=$out -DCMAKE_C_FLAGS="$CFLAGS -Wno-error=implicit-function-declaration" -DCMAKE_INSTALL_LIBDIR=lib
-    make
-    make install
-    cd ../../clickhouse
-    mkdir build
-    cd build
-    cmake .. -DCMAKE_INSTALL_PREFIX=$out -DCMAKE_C_FLAGS="$CFLAGS -Wno-error=implicit-function-declaration" -DCMAKE_INSTALL_LIBDIR=lib
-    make
-    echo "Make install cliuckhouse"
-    make install
-    cd ../../protobuf-kafka
-    mkdir build
-    cd build
-    echo "Building Protobuf-Kafka plugin"
-    cmake .. -DCMAKE_INSTALL_PREFIX=$out -DCMAKE_C_FLAGS="$CFLAGS -Wno-error=implicit-function-declaration" -DCMAKE_INSTALL_LIBDIR=lib
-    make
-    echo "Make install protobuf-kafka"
-    make install
-
+    srcRoot=$(cd .. && pwd)
+    for plugin in unirec clickhouse; do
+      echo "Building $plugin plugin..."
+      echo srcRoot is $srcRoot $(ls)
+      echo $(ls $srcRoot/extra_plugins/output)
+      cd "$srcRoot/extra_plugins/output/$plugin"
+      mkdir -p build && cd build
+      cmake .. \
+        -DCMAKE_INSTALL_PREFIX=$out \
+        -DCMAKE_C_FLAGS="$CFLAGS -Wno-error=implicit-function-declaration" \
+        -DCMAKE_INSTALL_LIBDIR=lib \
+        -DCMAKE_POLICY_VERSION_MINIMUM=3.5
+      make && make install
+    done
   '';
+
+
 
   meta = {
     description = "Flexible, high-performance NetFlow v5/v9 and IPFIX flow data collector designed to be extensible by plugins";
